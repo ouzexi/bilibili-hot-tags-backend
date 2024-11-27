2d9b37eaf655c19e7d668fb688b2412e9f5d0a3f
区泽熙 <1192102366@qq.com>
2024-11-27 21:27:01 +0800

    git add .

--- variable/origin.go
+++ variable/origin.go
@@ -1,5 +1,5 @@
 package variable
 
-var Origin string = "https://bilibili-hot-tags-git-master-ouzexis-projects.vercel.app"
+var Origin string = "https://bilibili-hot-tags-git-master-ouzexis-projects.vercel.app/"
 
 var IgnoreApi string = "/update-cookie"
